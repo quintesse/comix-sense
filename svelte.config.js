6432a9090884ad827551bd3abf27d8239fe85a3e
Tako Schotanus <tako@codejive.org>
2022-06-13 12:08:29 +0200

    build: Setting up Svelte config for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from "@sveltejs/adapter-static"; 
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -8,12 +10,13 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+        adapter: adapter({
+            pages: "public",
+            assets: "public"
+        }),
+        paths: {
+            base: dev ? "" : "/comix-sense",
+        }
 	}
 };
 
